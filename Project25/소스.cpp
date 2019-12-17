eed5e81e3607bf62f533f5d22e0bfca3992c84c7
User <User@DESKTOP-083QOIL>
2019-12-17 21:48:14 +0900

    초기 커밋.

---
+++ Project25/소스.cpp
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+
+
+int main()
+{
+	double change(int a);
+	double inch;
+	printf("인치를 입력하세요: ");
+	scanf("%lf", &inch);
+	printf("%f인치는 %.2fcm입니다.\n", inch, change(inch));
+
+	return 0;
+}
+
+double change(int a)
+{
+	double b;
+	b = (a * 2.54);
+	return (b);
+}
+
